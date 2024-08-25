a47ba2cf1c5fd2347bba02dfd53028cd6a920ac5
noale1 <123noa.levy@gmail.com>
2024-08-25 12:39:39 +0300

    fix routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+const path = require('path');
 
 // Order routes
 router.post('/orders', orderController.create_order);
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const path = require('path');
 
-// Product routes
-router.get('/test', (req, res) => res.json("this is first route"))
-// router.get('/products', productController.list_products); 
 router.post('/admin/products', productController.add_product);
 router.delete('/admin/products/:id', productController.delete_product);
 router.post('/admin/products/:id', productController.update_product);
