dfe8de635df650fc45daaadaff661ba97ed8af8f
ityk23 <ityk232001@gmail.com>
2024-09-28 18:19:13 +0300

    add product filter

--- routes/public/apiRoutes.js
+++ routes/public/apiRoutes.js
@@ -5,6 +5,7 @@ const locationController = require('../../controllers/locationController');
 const orderController = require('../../controllers/orderController');
 
 router.get('/getProducts', productController.get_products);
+router.get('/getCategories', productController.get_categories);
 
 router.get('/getMapsAPIKey', locationController.getApiKey);
 router.get('/locations',locationController.getLocations);
