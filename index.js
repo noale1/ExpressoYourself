b9c91e2a946d345cea4ddc8e4d2bf9557c0fc020
Daniel <danielvsi12@gmail.com>
2024-10-01 23:45:42 +0300

    fix(locations.html, products.html): resolve issues from merging.

--- index.js
+++ index.js
@@ -17,32 +17,6 @@ mongoose.connect(mongoAtlasUri)
     .then(() => console.log('Connected!'))
     .catch(e => console.log(e));
 
-// Handle routes
-const authRoutes = require('./routes/authRoutes.js');
-const userRoutes = require('./routes/userRoutes.js');
-const productRoutes = require('./routes/productRoutes.js');
-const orderRoutes = require('./routes/orderRoutes.js');
-const supplierRoutes = require('./routes/supplierRoutes.js');
-const locationRoutes = require('./routes/locationRoutes.js');
-
-app.use(express.json());
-
-app.use('/', authRoutes);
-app.use('/api', userRoutes);
-app.use('/products', productRoutes);
-app.use('/supplier', supplierRoutes);
-app.use('/locations', locationRoutes);
-app.use('/order', orderRoutes);
-
-app.get('/products', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages', 'products.html'));
-});
-
-app.get('/order', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages', 'order.html'));
-});
-
-// app.use(express.static('public'));
 const routes = require('./routes');
 app.use('/', routes);
     // Handle routes
