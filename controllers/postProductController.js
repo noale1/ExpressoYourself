cc2d53a903a2e983d775a27e03f0ee91a0f35151
Daniel <danielvsi12@gmail.com>
2024-10-04 20:59:21 +0300

    fix(postProductController.js) changed package to the correct one.

--- controllers/postProductController.js
+++ controllers/postProductController.js
@@ -1,23 +1,22 @@
 require('dotenv').config();
-const Twitter = require('twitter-api-v2').default;
+const { TwitterApi } = require('twitter-api-v2');
 
-const client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+const client = new TwitterApi({
+    appKey: process.env.API_KEY,
+    appSecret: process.env.API_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_SECRET,
 });
 
+const twitterClient = client.readWrite;
+
 const postProduct = async (product) => {
-    console.log("REACHED 1");
     const POST_TEMPLATE = `New product added: ${product.name} for $${product.price}!`;
-    console.log("REACHED 2");
 
     try {
-        await client.v2.tweet(POST_TEMPLATE);
-        console.log("Tweet posted successfully");
-    } catch (error) {
-        console.error("Error posting tweet:", error);
+        await twitterClient.v2.tweet(POST_TEMPLATE);
+    } catch (e) {
+        console.log(e)
     }
 };
 
