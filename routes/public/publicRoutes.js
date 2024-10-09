d29507120647ad06d4247f5a6c927f126604f908
noale1 <123noa.levy@gmail.com>
2024-10-09 19:02:02 +0300

    add 404 page

--- routes/public/publicRoutes.js
+++ routes/public/publicRoutes.js
@@ -6,28 +6,32 @@ const RELATIVE_PAGES_PATH = '../../views/pages';
 
 
 // home page
-router.get('/', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'home.html'))});
+router.get('/', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'home.html')) });
 
 // product page
-router.get('/products', (req, res) => {res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'products.html'))});
+router.get('/products', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'products.html')) });
 
 //About Page
-router.get('/about', (req, res) => {res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH,'AboutPage.html'));});
+router.get('/about', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'AboutPage.html')); });
 
 //ContactUs Page
-router.get('/contact', (req, res) => {res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH,'ContactUs.html'));});
+router.get('/contact', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'ContactUs.html')); });
 
+// 404 Page
+router.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, '404.html'));
+});
 
 
 // login and register pages
-router.get('/login', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'login.html'))});
-router.get('/register', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'register.html'))});
+router.get('/login', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'login.html')) });
+router.get('/register', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'register.html')) });
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
 // map page
-router.get('/locations', (req, res) => {res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH,'locations.html'));});
+router.get('/locations', (req, res) => { res.sendFile(path.join(__dirname, RELATIVE_PAGES_PATH, 'locations.html')); });
 
 
 
