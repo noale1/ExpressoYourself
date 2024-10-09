60bbd837dafe99e2d68827dee3d596405123e585
YoavShtresler <yoyoav2001@gmail.com>
2024-10-09 21:55:44 +0300

    token names

--- controllers/postProductController.js
+++ controllers/postProductController.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const { TwitterApi } = require('twitter-api-v2');
 
 const client = new TwitterApi({
-    appKey: process.env.API_KEY,
-    appSecret: process.env.API_SECRET,
-    accessToken: process.env.ACCESS_TOKEN,
-    accessSecret: process.env.ACCESS_SECRET,
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_SECRET,
 });
 
 const twitterClient = client.readWrite;
